125c51022adfe5df392cc6d5274656246aad77ac
Eimer <silbern.eimer@gmail.com>
2020-05-24 03:36:48 +0000

    Implement skelton cli app

---
+++ Makefile
@@ -0,0 +1,6 @@
+setup:
+	yarn
+build:
+	yarn build
+run: build
+	yarn runcli
---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { render } from "ink";
+import { Text, Box, Color } from "ink";
+
+main();
+
+function main() {
+    render(
+        <Box margin={1}>
+            <Text bold>Hello, React Ink CLI App!</Text>
+        </Box>
+    );
+}
